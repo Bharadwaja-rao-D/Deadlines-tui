58324c45f3aa56a3b1a3a74cd8cd62014bc89e89
bharadwaja-rao-d <cs20btech11012@iith.ac.in>
2022-09-01 15:36:04 +0530

    Uptodate

--- src/operations/display.rs
+++ src/operations/display.rs
@@ -118,6 +118,7 @@ fn construct_block(deadline: Deadline) -> Paragraph<'static> {
         .title(format!("{}", deadline.course))
         .borders(Borders::ALL);
 
+    //TODO: Find a way to remove the completed things
     if completed {
         block = block.border_style(Style::default().fg(Color::Red));
     } else if nearer {
@@ -137,6 +138,8 @@ fn construct_block(deadline: Deadline) -> Paragraph<'static> {
 pub fn actual_render<B: Backend>(frame: &mut Frame<B>, render_info: &mut RenderInfo) {
     let visible_deadlines = &mut render_info.visible_deadlines;
     let chunks = &render_info.chunks;
+    //Constructed a custom iterator for visible_deadlines which returns a batch of deadlines based
+    //on the value of VISIBLE
     if let Some(deadlines) = visible_deadlines.next() {
         for (i, deadline) in deadlines.into_iter().enumerate() {
             let paragraph = construct_block(deadline);
--- src/operations/fetch.rs
+++ src/operations/fetch.rs
@@ -1,1 +1,36 @@
 //TODO: Uses gclassroom apis to fetch deadlines
+
+use std::fs;
+
+use oauth2::basic::BasicClient;
+use serde::{Deserialize, Serialize};
+
+use crate::operations::Info;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Credentials {
+    client_id: String,
+    project_id: String,
+    auth_uri: String,
+    token_uri: String,
+    auth_provider_x509_cert_url: String,
+    client_secret: String,
+    redirect_uris: Vec<String>,
+}
+
+pub fn fetch_deadlines() -> Option<Info> {
+    let info = fs::read_to_string("credentials.json").unwrap();
+    let info: Credentials = serde_json::from_str(&info).unwrap();
+
+    let url = &info.redirect_uris[0];
+
+    let client = BasicClient::new(
+        oauth2::ClientId::new(info.client_id),
+        Some(oauth2::ClientSecret::new(info.client_secret)),
+        oauth2::AuthUrl::new(info.auth_uri).unwrap(),
+        Some(oauth2::TokenUrl::new(info.token_uri).unwrap()),
+    )
+    .set_redirect_uri(oauth2::RedirectUrl::new(url.to_string()).unwrap());
+
+    return None;
+}
